259f92cc9d04c7fdb4b0127ff7e7e2d4032ba6fa
Serperio <serperiox@gmail.com>
2022-01-29 19:16:26 -0300

    AudioManager Implementado

---
+++ Assets/Scripts/TestingMusic.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingMusic : MonoBehaviour
+{
+    public AudioClip a;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioManager.Instance.PlayMusic(a);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("m")){
+            AudioManager.Instance.MuteMusic();
+        }
+        if (Input.GetKey("n"))
+        {
+            AudioManager.Instance.PlayMusic(a);
+        }
+    }
+}

1c4574e60a160316e84ce6ed69e8674821fa6ba1
brauliosba <braulio.baldeon07@gmail.com>
2022-01-29 23:19:03 -0500

    sun fix

--- Assets/Scripts/SunAnimation.cs
+++ Assets/Scripts/SunAnimation.cs
@@ -6,7 +6,6 @@ using DG.Tweening;
 
 public class SunAnimation : MonoBehaviour
 {
-    [SerializeField] [Range(2.0f, 70.0f)] private float time = 4.29f;
     Material material;
     bool isAnimated = false;
     Sequence sequence;
@@ -23,21 +22,21 @@ public class SunAnimation : MonoBehaviour
     {
         if (!isAnimated)
         {
-            DOTween.To(() => material.GetFloat("_TransparencyA"), x => material.SetFloat("_TransparencyA", x), 0.8f, time).OnPlay(() =>
+            DOTween.To(() => material.GetFloat("_TransparencyA"), x => material.SetFloat("_TransparencyA", x), 0.8f, 50f).OnPlay(() =>
             {
                 material.SetFloat("_Phase", 1);
                 isAnimated = true;
             }).OnComplete(() => material.SetFloat("_TransparencyA", -0.8f)).SetEase(Ease.Linear);
 
             sequence = DOTween.Sequence();
-            sequence.AppendInterval(time - 3f)
-                .Append(DOTween.To(() => material.GetFloat("_TransparencyB"), x => material.SetFloat("_TransparencyB", x), -0.8f, time)).OnComplete(() => material.SetFloat("_TransparencyB", 0.8f)).SetEase(Ease.Linear);
+            sequence.AppendInterval(20f)
+                .Append(DOTween.To(() => material.GetFloat("_TransparencyB"), x => material.SetFloat("_TransparencyB", x), -0.8f, 50f)).OnComplete(() => material.SetFloat("_TransparencyB", 0.8f)).SetEase(Ease.Linear);
 
             sequence = DOTween.Sequence();
-            sequence.AppendInterval(time - 1.5f).Append(DOTween.To(() => num, x => num = x, 1, 0.2f).OnPlay(() => material.SetFloat("_Phase", 0)));
+            sequence.AppendInterval(45f).Append(DOTween.To(() => num, x => num = x, 1, 0.2f).OnPlay(() => material.SetFloat("_Phase", 0)));
 
             sequence = DOTween.Sequence();
-            sequence.AppendInterval(time + .5f).Append(DOTween.To(() => num, x => num = x, 0, 0.2f).OnPlay(() =>
+            sequence.AppendInterval(55f).Append(DOTween.To(() => num, x => num = x, 0, 0.2f).OnPlay(() =>
             {
                 isAnimated = false;
             }));
